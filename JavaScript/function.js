dba2a6514a8d83689360cf000c836a524200377d
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-20 18:49:59 +0300

    comment code

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -2,17 +2,18 @@ console.log('functions page');
 
 function login(){
     console.log('Login function Run!!! \n this is first function Test');
-    let name = prompt("Write your name!");
+    // ** Remove comment sign to try this code ** 
+    /*let name = prompt("Write your name!");
     alert(`Welcome ${name}`);
     console.log(`Tanks ${name} to write your name`)
     let qui =confirm(`${name} are you want to continue!!!!!`)
     if(qui){
         console.log(`Good choice,${name}`);
-        alert(`Good choice,${name}`);
+        //alert(`Good choice,${name}`);
     }else{
         console.log(`Bye,${name}`);
-        alert(`Bye,${name}`)
-    }
+        //alert(`Bye,${name}`)
+    }*/
 }
 
 login();
