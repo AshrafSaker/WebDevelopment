3c9d39be12c94af0a61f0cdc991ec474fd3163b7
Ashraf Saker <engashrafsaker@gmail.com>
2024-08-20 18:38:38 +0300

    order
    console log
    alert
    confirm
    if & else

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -2,6 +2,17 @@ console.log('functions page');
 
 function login(){
     console.log('Login function Run!!! \n this is first function Test');
+    let name = prompt("Write your name!");
+    alert(`Welcome ${name}`);
+    console.log(`Tanks ${name} to write your name`)
+    let qui =confirm(`${name} are you want to continue!!!!!`)
+    if(qui){
+        console.log(`Good choice,${name}`);
+        alert(`Good choice,${name}`);
+    }else{
+        console.log(`Bye,${name}`);
+        alert(`Bye,${name}`)
+    }
 }
 
 login();
